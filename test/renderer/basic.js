54b9ca7b793360092b34b6a4fbc4dafc3022d884
Nico Rehwaldt <git_nikku@nixis.de>
2013-06-11 17:13:29 +0200

    tests(renderer): add full render scenario

--- test/renderer/basic.js
+++ test/renderer/basic.js
@@ -199,6 +199,28 @@ return describe('Basic Renderer Functionality', function() {
 
   });
 
+  it('should render complete render test', function() {
+
+    var rendered = false;
+
+    var bpmn = new Bpmn();
+    bpmn.renderUrl("resources/renderer-test.bpmn", {
+      diagramElement : "canvas"
+    }).then(function (bpmn) {
+        rendered = true;
+    });
+
+    waitsFor(function() {
+      return rendered;
+    }, "Rendering never completed", 10000);
+
+    runs(function () {
+      expect(bpmn.definitionRenderer).toBeDefined();
+      // there should be 9 rect task shapes
+      // expect(helper.findChildrenByType(bpmn.definitionRenderer.gfxGroup, "rect").length).toBe(9);
+    });
+
+  });
 });
 
 });
